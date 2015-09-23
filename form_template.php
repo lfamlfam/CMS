de1c08e91eec3fab94fce52d26a1f166f3c449fa
lfamlfam <lfamlfam@gmail.com>
2015-09-22 22:36:26 -0300

    Create form_template.php

---
+++ form_template.php
@@ -0,0 +1,13 @@
+<html>
+	<body>
+		<br><br><br>
+		<form method='post'>
+			Login <input type='text' name='login'>
+			<br>
+			Senha <input type='password' name='senha'>
+			<br>
+			<input type='submit' value='Login' 
+			name='enviar'>
+		</form>
+	</body>
+</html>
