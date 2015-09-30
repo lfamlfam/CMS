6f2edbc8245ecef421052d7f83e632f0735ed3d5
lfamlfam <lfamlfam@gmail.com>
2015-09-29 22:07:58 -0300

    Update php_oo.php

--- php_oo.php
+++ php_oo.php
@@ -1,12 +1,43 @@
 <?php
-class IMC{
+abstract class IMC{
+
+	const BASE=2;
 	private $peso;
 	private $altura;
 	public $imc;
 	
-	public function __construct($peso, $altura){
-		$this->imc = $peso / ($altura^2);
+	public function __construct(){
+	
+	}
+	
+	public function __call($metodo, $parametro){
+		echo "<br><br>Me desculpe, o m&eacute;todo
+				$metodo n&atilde;o existe!<br><br>";
+	}
+	
+	public function __set($atributo, $val){
+		$this->$atributo = $val;
+	}
+	
+	public function __get($atributo){
+		return $this->$atributo;
+	}
+	
+	public function Calcula(){
+		$this->imc = $this->peso / ($this->altura^self::BASE);
 		echo $this->exibeParametro();
+	}
+	
+	public function setPeso($peso){
+		$this->peso = $peso;
+	}
+	
+	public function getPeso(){
+		return $this->peso;
+	}
+	
+	public function setAltura($altura){
+		$this->altura = $altura;
 	}
 	
 	private function exibeParametro(){
@@ -27,10 +58,35 @@ class IMC{
 		echo "\n".'Objeto foi destruido com sucesso!!';
 	}
 }
-$objIMC = new IMC(70,1.82);
+class IMCFilha extends IMC{
+	private $var;
+	
+	function __construct(){
+		parent::__construct();
+		echo "<br>Classe Filha<br>";
+	}
+}
+
+$objIMCFilha = new IMCFilha;
+$objIMCFilha->__set('peso',90);
+echo "<br>O peso &eacute;: ".
+		$objIMCFilha->__get('peso')."<br>";
+
+/*
+$objIMC = new IMC;
+$objIMC->__set('peso',75);
+$objIMC->__set('altura',1.81);
+$objIMC->nomeTeste();
+echo "<br>Const: ".$objIMC::BASE."<br>";
+echo "<br>Const: ".IMC::BASE."<br>";
+//$objIMC->setPeso(78);
+//$objIMC->setAltura(1.82);
+$objIMC->Calcula();
+echo "<br> O peso &eacute;: ".$objIMC->__get('peso');
 echo "<pre>";
 var_dump($objIMC);
 unset($objIMC);
 
 echo "\nTermino do script</pre>";
+*/
 ?>
